6690485fa6578b01883ce4d81252640b22ad0c78
jamalm <bluedocjamal@hotmail.co.uk>
2015-09-30 15:23:57 +0000

    conditions and input added

--- palin.py
+++ palin.py
@@ -1,3 +1,7 @@
-word = input("Enter a word: \n")
-word = word[::-1]
-print(word)
+word = input("Enter a Palindrome: \n")
+
+if str(word.lower()) == str(word.lower())[::-1]:
+  print("True")
+
+else:
+  print("False")
